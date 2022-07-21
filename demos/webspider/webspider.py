382908d5acf5eab9f855b3095a825dfbeb18fdb9
Kang <2881570+kangni@users.noreply.github.com>
2022-07-21 13:19:47 +0800

    update spider

--- demos/webspider/webspider.py
+++ demos/webspider/webspider.py
@@ -49,19 +49,24 @@ def get_links(html):
 
 
 async def main():
+    # 创建 url 队列
+    # 工作区、完成区、出错区
     q = queues.Queue()
     start = time.time()
     fetching, fetched, dead = set(), set(), set()
 
     async def fetch_url(current_url):
+        # 忽略正在爬取（工作区）的 url
         if current_url in fetching:
             return
 
         print("fetching %s" % current_url)
         fetching.add(current_url)
         urls = await get_links_from_url(current_url)
+        # 爬取完成放进完成区
         fetched.add(current_url)
 
+        # 筛选新的，并加入到队列
         for new_url in urls:
             # Only follow links beneath the base URL
             if new_url.startswith(base_url):
@@ -77,12 +82,16 @@ async def main():
                 print("Exception: %s %s" % (e, url))
                 dead.add(url)
             finally:
+                # 完成后调用 task_done，队列中的未完成任务计数将减 1， 与 q.get 一一对应。
+                # 为的是配合 q.join， 当计数为 0 时， q.join 将结束等待。
                 q.task_done()
 
+    # 为队列添加第一个 url （任务）
     await q.put(base_url)
 
     # Start workers, then wait for the work queue to be empty.
     workers = gen.multi([worker() for _ in range(concurrency)])
+    # 使用 join 阻塞等待，直到队列被清空才会恢复，继续往下走
     await q.join(timeout=timedelta(seconds=300))
     assert fetching == (fetched | dead)
     print("Done in %d seconds, fetched %s URLs." % (time.time() - start, len(fetched)))
